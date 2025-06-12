86bcb6a320f5dd8b31490e6ce79bc5a4600d9ca5
mur3l <tippitauno@gmail.com>
2025-06-12 22:44:12 +0300

    AppDbContext.cs -> Typo -> COMMIT

--- AdvancedAjax/Data/AppDbContext.cs
+++ AdvancedAjax/Data/AppDbContext.cs
@@ -2,13 +2,10 @@
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext()
-        {
-        }
+        public AppDbContext() { }
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
-        {
-        }
+        { }
         public virtual DbSet<City> Cities { get; set; }
         public virtual DbSet<Country> Countries { get; set; }
 
