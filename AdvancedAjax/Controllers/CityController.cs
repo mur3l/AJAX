c7763ed74a94f929a79d423f5fcb8d8ec56c6191
mur3l <tippitauno@gmail.com>
2025-06-12 21:43:07 +0300

    CityController - Fixes to forming & Added missing code -> COMMIT

--- AdvancedAjax/Controllers/CityController.cs
+++ AdvancedAjax/Controllers/CityController.cs
@@ -13,9 +13,10 @@ namespace AdvancedAjax.Controllers
 
         public IActionResult Index()
         {
-            List<City> Cities;
-            Cities = _context.Cities.ToList();
-            return View(Cities);
+            List<City> cities = _context.Cities
+                 .Include(c => c.Country)
+                 .ToList();
+            return View(cities);
         }
 
         [HttpGet]
@@ -37,20 +38,20 @@ namespace AdvancedAjax.Controllers
         }
 
         [HttpGet]
-        public IActionResult Details(int Id)
+        public IActionResult Details(int id)
         {
             City City = _context.Cities
-                .Where(c => c.Id == Id).FirstOrDefault();
+                .Include(co => co.Country)
+                .Where(c => c.Id == id).FirstOrDefault();
+
             return View(City);
         }
 
         [HttpGet]
         public IActionResult Edit(int Id)
         {
-            City City = _context.Cities
-                .Where(c => c.Id == Id).FirstOrDefault();
+            City City = _context.Cities.Where(c => c.Id == Id).FirstOrDefault();
             ViewBag.Countries = GetCountries();
-
             return View(City);
         }
 
@@ -66,21 +67,31 @@ namespace AdvancedAjax.Controllers
         }
 
         [HttpGet]
-        public IActionResult Delete(int Id)
+        public IActionResult Delete(int id)
         {
-            City City = _context.Cities
-                .Where(c => c.Id == Id).FirstOrDefault();
-
-            return View(City);
+            City city = _context.Cities
+                .Include(co => co.Country)
+                .Where(c => c.Id == id).FirstOrDefault();
+            return View(city);
         }
 
         [ValidateAntiForgeryToken]
         [HttpPost]
-        public IActionResult Delete(City City)
+        public IActionResult Delete(City city)
+
         {
-            _context.Attach(City);
-            _context.Entry(City).State = EntityState.Deleted;
-            _context.SaveChanges();
+            try
+            {
+                _context.Attach(city);
+                _context.Entry(city).State = EntityState.Deleted;
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _context.Entry(city).Reload();
+                ModelState.AddModelError("", ex.InnerException.Message);
+                return View(city);
+            }
             return RedirectToAction(nameof(Index));
 
         }
@@ -125,9 +136,11 @@ namespace AdvancedAjax.Controllers
         {
             if (countryId == 0)
                 return "";
+
             string strCountryName = _context.Countries
-                .Where(ct => ct.Id == countryId)
-                .Select(nm => nm.Name).Single().ToString();
+                .Where(c => c.Id == countryId)
+                .Select(n => n.Name).Single().ToString();
+
             return strCountryName;
         }
     }
